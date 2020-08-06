7743588c15db8787bad23b337174db10ea737eb0
Ruben Dominguez <rdominguezp@gmail.com>
2020-08-06 17:59:49 +0200

    añadí comillas simples a los links

--- scraper.py
+++ scraper.py
@@ -3,10 +3,10 @@ import lxml.html as html
 
 HOME_URL = 'https://www.elpais.com/'
 
-XPATH_LINK_TO_ARTICLE = //*[@id="fusion-app"]/div[5]/div[2]/div[1]/div[2]/article[2]/h2/a/text()
-XPATH_TITLE = //h1[@class="a_t | font_secondary color_gray_ultra_dark"]/text()
-XPATH_SUMMARY = //*[@id="article_header"]/h2/text()
-XPATH_BODY = //div[@class="a_b article_body | color_gray_dark"]/p/text()
+XPATH_LINK_TO_ARTICLE = '//*[@id="fusion-app"]/div[5]/div[2]/div[1]/div[2]/article[2]/h2/a/text()'
+XPATH_TITLE = '//h1[@class="a_t | font_secondary color_gray_ultra_dark"]/text()'
+XPATH_SUMMARY = '//*[@id="article_header"]/h2/text()'
+XPATH_BODY = '//div[@class="a_b article_body | color_gray_dark"]/p/text()'
 
 def parse_home ():
     try:
